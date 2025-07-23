5c8208af134e64787bb91b8a9b800f85e1898fc2
Divyam Dubey <61962923+divyam2207@users.noreply.github.com>
2025-07-23 00:44:42 -0400

    Time: 207 ms (57.42%), Space: 19.1 MB (87.74%) - LeetHub

---
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maximumGain(self, s: str, x: int, y: int) -> int:
+        def removePairs(pair, score):
+            nonlocal s
+            res = 0
+            stack = []
+            for each in s:
+                if each == pair[1] and stack and stack[-1] == pair[0]:
+                    stack.pop()
+                    res += score
+                else:
+                    stack.append(each)
+
+            s = "".join(stack)
+            return res
+        
+        res = 0
+        pair = "ab" if x>y else "ba"
+        res += removePairs(pair, max(x,y))
+        res += removePairs(pair[::-1], min(x,y))
+        return res
